ec1d347246bf07631920a7e7183eaecb9cbd8162
Egil Rian <egil.rian@expeditors.com>
2021-01-08 10:16:05 -0800

    I had fun day with my father.

---
+++ src/Counting.java
@@ -0,0 +1,12 @@
+public class Counting {
+    public static void main(String[] args){
+        System.out.println("Meow, they said.");
+        System.out.println("args[0] = " + args[0]);
+        System.out.println("Hello " + args[2]);
+        int size = args.length;
+        for (int i = 0; i < size; i++){
+            System.out.println(args[i]);
+
+        }
+    }
+}
