9d4221d39cbc040d4d7142129561462a26e060c8
Egil Rian <egil.rian@expeditors.com>
2021-01-11 22:35:27 -0800

    The project print display is completely identical to the project display on canvas. The area function is correct, and the axes are displayed correctly. Final step is to test this class method by method.

--- src/SidePlot.java
+++ src/SidePlot.java
@@ -6,9 +6,9 @@ public class SidePlot {
     public static void main(String[] Args) {
         System.out.println("Sideways Plot:");
         plotXSquaredPlus2(-5, 5);
-//        plotAbsXTimes3(-6, 6);
-//        plotNegXSquaredPlus25(-5, 5);
-//        plotSinWave(-11, 11);
+        plotAbsXTimes3(-6, 6);
+        plotNegXSquaredPlus25(-5, 5);
+        plotSinWave(-11, 11);
     }
 
     private static void plotXSquaredPlus2(int minX, int maxX) {
@@ -21,11 +21,57 @@ public class SidePlot {
                 highest_y_value = y;
             }
             for (int h = -2; h < y; h++) {
+                area ++;
                 if (h < 0 & x == 0) {
                     System.out.print("-");
+                    area --;
                 }
                 if (h < 0 & x != 0) {
                     System.out.print(" ");
+                    area --;
+                }
+                if (h == 0 & x != 0) {
+                    System.out.print("|");
+                }
+                if (h == 0 & x == 0) {
+                    System.out.print("+");
+                }
+                if (h > 0 & x != 0) {
+                    System.out.print(FILL_CHARACTER);
+                }
+                if (h > 0 & x == 0) {
+                    System.out.print("-");
+                }
+            }
+            System.out.print(PLOT_CHARACTER);
+            if (x == 0) {
+                for (int i = 0; i < highest_y_value - y; i++) {
+                    System.out.print("-");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("The area under the plot is " + area);
+        System.out.println();
+    }
+
+    private static void plotAbsXTimes3(int minX, int maxX) {
+        System.out.println("y = |x|*3, where " + minX + "<=x<=" + maxX);
+        int area = 0;
+        int highest_y_value = 0;
+        for (int x = minX; x <= maxX; x++) {
+            int y = Math.abs(x)*3;
+            if (y >= highest_y_value) {
+                highest_y_value = y;
+            }
+            for (int h = -2; h < y; h++) {
+                if (h < 0 & x == 0) {
+                    System.out.print("-");
+                    area --;
+                }
+                if (h < 0 & x != 0) {
+                    System.out.print(" ");
+                    area --;
                 }
                 if (h == 0 & x != 0) {
                     System.out.print("|");
@@ -53,31 +99,45 @@ public class SidePlot {
         System.out.println();
     }
 
-    private static void plotAbsXTimes3(int minX, int maxX) {
-        System.out.println("y = |x|*3, where " + minX + "<=x<=" + maxX);
-        int area = 0;
-        for (int x = minX; x <= maxX; x++) {
-            int y = Math.abs(x)*3;
-            for (int h = 0; h < y; h++) {
-                System.out.print(FILL_CHARACTER);
-                area += 1;
-            }
-            System.out.println(PLOT_CHARACTER);
-        }
-        System.out.println("The area under the plot is " + area);
-        System.out.println();
-    }
-
     private static void plotNegXSquaredPlus25(int minX, int maxX) {
         System.out.println("y = -(x^2)+25, where " + minX + "<=x<=" + maxX);
         int area = 0;
+        int highest_y_value = 0;
         for (int x = minX; x <= maxX; x++) {
             int y = -(x*x)+25;
-            for (int h = 0; h < y; h++) {
-                System.out.print(FILL_CHARACTER);
-                area += 1;
+            if (y >= highest_y_value) {
+                highest_y_value = y;
             }
-            System.out.println(PLOT_CHARACTER);
+            for (int h = -2; h < y; h++) {
+                if (h < 0 & x == 0) {
+                    System.out.print("-");
+                    area --;
+                }
+                if (h < 0 & x != 0) {
+                    System.out.print(" ");
+                    area --;
+                }
+                if (h == 0 & x != 0) {
+                    System.out.print("|");
+                }
+                if (h == 0 & x == 0) {
+                    System.out.print("+");
+                }
+                area++;
+                if (h > 0 & x != 0) {
+                    System.out.print(FILL_CHARACTER);
+                }
+                if (h > 0 & x == 0) {
+                    System.out.print("-");
+                }
+            }
+            System.out.print(PLOT_CHARACTER);
+            if (x == 0) {
+                for (int i = 0; i < highest_y_value - y; i++) {
+                    System.out.print("-");
+                }
+            }
+            System.out.println();
         }
         System.out.println("The area under the plot is " + area);
         System.out.println();
@@ -86,13 +146,42 @@ public class SidePlot {
     private static void plotSinWave(int minX, int maxX) {
         System.out.println("y = 20sin(.5x)+20, where " + minX + "<=x<=" + maxX);
         int area = 0;
+        int highest_y_value = 0;
         for (int x = minX; x <= maxX; x++) {
-            int y = (int) (20 * Math.sin(0.5*x) + 20);
-            for (int h = 0; h < y; h++) {
-                System.out.print(FILL_CHARACTER);
-                area += 1;
+            int y = (int) (20*Math.sin(.5 * x) + 20);
+            if (y >= highest_y_value) {
+                highest_y_value = y;
             }
-            System.out.println(PLOT_CHARACTER);
+            for (int h = -2; h < y; h++) {
+                if (h < 0 & x == 0) {
+                    System.out.print("-");
+                    area --;
+                }
+                if (h < 0 & x != 0) {
+                    System.out.print(" ");
+                    area --;
+                }
+                if (h == 0 & x != 0) {
+                    System.out.print("|");
+                }
+                if (h == 0 & x == 0) {
+                    System.out.print("+");
+                }
+                area++;
+                if (h > 0 & x != 0) {
+                    System.out.print(FILL_CHARACTER);
+                }
+                if (h > 0 & x == 0) {
+                    System.out.print("-");
+                }
+            }
+            System.out.print(PLOT_CHARACTER);
+            if (x == 0) {
+                for (int i = 0; i < highest_y_value - y; i++) {
+                    System.out.print("-");
+                }
+            }
+            System.out.println();
         }
         System.out.println("The area under the plot is " + area);
         System.out.println();
