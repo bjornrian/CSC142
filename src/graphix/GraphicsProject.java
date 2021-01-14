4757d0d0e4937538b7e0d2555f3d80f5ef1dcd2e
Egil Rian <egil.rian@expeditors.com>
2021-01-14 15:45:04 -0800

    My code now projects a shape that is basically identical (line spacing is different but that is allowed) to the rubric model. Now the next challenge is to change the color gradually from green to blue with rgb selectors, who's values will be increased/decreased during each repetition of the loop.

--- src/graphix/GraphicsProject.java
+++ src/graphix/GraphicsProject.java
@@ -7,15 +7,28 @@ public class GraphicsProject {
         Graphics graphics = panel.getGraphics();
         graphics.setColor(Color.BLUE);
 
-        int x1 = 330;
+        int x1 = 0;
+        int y1 = 600;
+        int x2 = 300;
+        int y2 = 300;
+        for (int i = 0; i < 310; i += 10) {
+            graphics.drawLine(x1 + i, y1 - i, x2 + i, y2 + i);
+            graphics.drawLine(x1 + i, i, x2 - i, y2 + i);
+            graphics.drawLine(x1 + i, i, x2 + i, y2 - i);
+            graphics.drawLine(600 - i, 600 - i, x2 + i, y2 - i);
+
+
+        /*
+        int x1 = 300;
         int y1 = 300;
         int x2 = 300;
         int y2 = 600;
-        for (int i = 0; i < 300; i += 30) {
+        for (int i = 0; i < 310; i += 10) {
             graphics.drawLine(x1 + i, y1, x2, y2 - i);
-            graphics.drawLine(x1 - 60 - i, 300, 300, y2 - i);
-            graphics.drawLine(x1 - 60 - i, 300, 300, i);
+            graphics.drawLine(x1 - 10 - i, 300, 300, y2 - i);
+            graphics.drawLine(x1 - 10 - i, 300, 300, i);
             graphics.drawLine(x1 + i, 300, 300, i);
+         */
         }
     }
 }
