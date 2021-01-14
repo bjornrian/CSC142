77263814075ddb42934d9eb5a6f056e6b92f261c
Egil Rian <egil.rian@expeditors.com>
2021-01-14 15:21:39 -0800

    Now I have created four of these curves that create an interesting shape with points on the midpoints of the canvas sides.

--- src/graphix/GraphicsProject.java
+++ src/graphix/GraphicsProject.java
@@ -3,16 +3,19 @@ import java.awt.*;
 
 public class GraphicsProject {
     public static void main(String[] args) {
-        DrawingPanel panel = new DrawingPanel(1000, 1000);
+        DrawingPanel panel = new DrawingPanel(600, 600);
         Graphics graphics = panel.getGraphics();
-        graphics.setColor(Color.GREEN);
+        graphics.setColor(Color.BLUE);
 
-        int x1 = 100;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 1000;
-        for (int i = 0; i < 1000; i += 100) {
+        int x1 = 330;
+        int y1 = 300;
+        int x2 = 300;
+        int y2 = 600;
+        for (int i = 0; i < 300; i += 30) {
             graphics.drawLine(x1 + i, y1, x2, y2 - i);
+            graphics.drawLine(x1 - 60 - i, 300, 300, y2 - i);
+            graphics.drawLine(x1 - 60 - i, 300, 300, i);
+            graphics.drawLine(x1 + i, 300, 300, i);
         }
     }
 }
