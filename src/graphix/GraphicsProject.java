7c26a9053a3e6f38439768dce1b95d26e5dd1c07
Egil Rian <egil.rian@expeditors.com>
2021-01-14 21:02:30 -0800

    I currently have two colors on the shape, although I have not been able to make the colors fade to one another as the project model does. I will figure this out tomorrow, most likely during class.

--- src/graphix/GraphicsProject.java
+++ src/graphix/GraphicsProject.java
@@ -1,20 +1,29 @@
 package graphix;
-import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Color;
 
 public class GraphicsProject {
     public static void main(String[] args) {
         DrawingPanel panel = new DrawingPanel(600, 600);
         Graphics graphics = panel.getGraphics();
-        graphics.setColor(Color.BLUE);
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0, 0, 600, 600);
 
         int x1 = 0;
         int y1 = 600;
         int x2 = 300;
         int y2 = 300;
+        int f = 0;
+
         for (int i = 0; i < 310; i += 10) {
+            f -= 3;
+            graphics.setColor(new Color(100 + f, 255, 224)); //light blue to lime
             graphics.drawLine(x1 + i, y1 - i, x2 + i, y2 + i);
+            graphics.setColor(new Color(20, 255, 224)); //light blue to lime
             graphics.drawLine(x1 + i, i, x2 - i, y2 + i);
+            graphics.setColor(new Color(82, 255, 38)); //lime to light blue
             graphics.drawLine(x1 + i, i, x2 + i, y2 - i);
+            graphics.setColor(new Color(82, 255, 38)); //lime to light blue
             graphics.drawLine(600 - i, 600 - i, x2 + i, y2 - i);
 
 
