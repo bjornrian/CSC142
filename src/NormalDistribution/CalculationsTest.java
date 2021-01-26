072df51e5b75e7ef0f2998cbaa3f86e18726402b
Egil Rian <egil.rian@expeditors.com>
2021-01-26 04:34:08 -0800

    Everything about the project seems good so far except for the restriction code for the user input. When valid values are entered, it takes three tries to get the output, and when the wrong values are entered, it takes two times to get an error message. This bug is too confusing so I will sleep now at 4:33....

--- src/NormalDistribution/CalculationsTest.java
+++ src/NormalDistribution/CalculationsTest.java
@@ -8,8 +8,15 @@ public class CalculationsTest {
     public void testYValue() {
         System.out.println("Begin test");
         Calculations.findYValue(0);
-        Calculations.findYValue(1);
+        Calculations.findYValue(50);
+        Calculations.findYValue(54);
+        Calculations.findYValue(100);
+        Calculations.findYValue(200);
+        Calculations.findYValue(300);
         Calculations.findYValue(350);
+        Calculations.findYValue(400);
+        Calculations.findYValue(500);
+        Calculations.findYValue(600);
         Calculations.findYValue(700);
         System.out.println("End test");
     }
--- src/NormalDistribution/UserComms.java
+++ src/NormalDistribution/UserComms.java
@@ -16,6 +16,10 @@ public class UserComms {
         System.out.println("0. EXIT the program");
         System.out.println();
         System.out.println("Enter your choice: (integer, 0-4)");
+        while (!userIn.hasNextInt() || userIn.nextInt() < 0 || userIn.nextInt() > 4) {
+                userIn.nextLine();
+                System.out.println("Error - choose an integer between 0-4:");
+        }
         int choice = userIn.nextInt();
         switch (choice) {
         case 0:
@@ -23,11 +27,19 @@ public class UserComms {
             System.exit(0);
         case 1:
             System.out.println("Enter x value:");
+            while (!userIn.hasNextInt() || userIn.nextInt() < 0 || userIn.nextInt() > 700) {
+                userIn.nextLine();
+                System.out.println("Error - choose an integer between 0-700:");
+            }
             int value = userIn.nextInt();
             Calculations.findYValue(value);
             break;
         case 2:
             System.out.println("Enter desired number of std dev's (integer, 1-4):");
+            while (!userIn.hasNextInt() || userIn.nextInt() < 0 || userIn.nextInt() > 4) {
+                userIn.nextLine();
+                System.out.println("Error - choose an integer between 0-4:");
+            }
             int numStdDevsArea = userIn.nextInt();
             Calculations.estimateArea(numStdDevsArea);
             break;
@@ -36,6 +48,10 @@ public class UserComms {
             break;
         case 4:
             System.out.println("Enter desired number of std dev's (integer, 1-4):");
+            while (!userIn.hasNextInt() || userIn.nextInt() < 0 || userIn.nextInt() > 4) {
+                userIn.nextLine();
+                System.out.println("Error - choose an integer between 0-4:");
+            }
             int numStdDevs = userIn.nextInt();
             DrawCurve.drawShadedCurve(numStdDevs);
             break;
