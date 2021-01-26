a18fc377f8f1e5c788d3e60947a2134d0d435d80
Egil Rian <egil.rian@expeditors.com>
2021-01-26 01:47:43 -0800

    Calculations.java seems to work fine so far, and I have managed to format the output so that only 4 places after the decimal are shown in the UI.

--- src/NormalDistribution/Calculations.java
+++ src/NormalDistribution/Calculations.java
@@ -1,9 +1,15 @@
 package NormalDistribution;
 
+import java.text.DecimalFormat;
+
 public class Calculations {
     public static void findYValue(int x) {
+        String pattern = "#.####";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
         float yValue = (float) (1/(100 * (Math.sqrt(2 * Math.PI))) * Math.pow(Math.E, - (Math.pow((x - 350), 2)) / 20000));
-        System.out.println("The y-value attributed to this x-value is " + yValue);
+        String format = decimalFormat.format(yValue);
+        System.out.println("The y-value attributed to this x-value is " + format);
+
     }
 
     public static void estimateArea(int numDev) {
--- src/NormalDistribution/DrawCurve.java
+++ src/NormalDistribution/DrawCurve.java
@@ -10,8 +10,9 @@ public class DrawCurve {
         Graphics graphics = panel2.getGraphics();
         graphics.setColor(Color.red);
         for (int x = 0; x <= 700; x++) {
-            int y = (int) Math.round(-(1/(100 * (Math.sqrt(2 * Math.PI))) * Math.pow(Math.E, - (Math.pow((x - 350), 2)) / 20000)) * 120000 + 500);
-            graphics.drawLine(x, y, x+1, y+1);
+            int y = (int) Math.round(1/(100 * (Math.sqrt(2 * Math.PI))) * Math.pow(Math.E, -(Math.pow((x - 350), 2)) / 20000));
+            System.out.println(y); //test only
+            graphics.drawLine(x, y, x+1,y+1);
         }
     }
 }
