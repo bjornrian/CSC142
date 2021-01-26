37bf8e0a771757c18e3d386937d0ef09b78e6206
Egil Rian <egil.rian@expeditors.com>
2021-01-26 03:20:29 -0800

    Figured out the curve. Multiply equation by 125,000 (500/0.004) and subtract 500 so that the curve is on the canvas.

--- src/NormalDistribution/DrawCurve.java
+++ src/NormalDistribution/DrawCurve.java
@@ -1,5 +1,4 @@
 package NormalDistribution;
-import NormalDistribution.DrawingPanel;
 
 import java.awt.Graphics;
 import java.awt.Color;
@@ -9,10 +8,11 @@ public class DrawCurve {
         NormalDistribution.DrawingPanel panel2 = new NormalDistribution.DrawingPanel(700, 500);
         Graphics graphics = panel2.getGraphics();
         graphics.setColor(Color.red);
+
+        //125000 value comes from 500/0.004 (Height/Highest y-value)
         for (int x = 0; x <= 700; x++) {
-            int y = (int) Math.round(1/(100 * (Math.sqrt(2 * Math.PI))) * Math.pow(Math.E, -(Math.pow((x - 350), 2)) / 20000));
-            System.out.println(y); //test only
-            graphics.drawLine(x, y, x+1,y+1);
+            double y = -(1/(100 * (Math.sqrt(2 * Math.PI))) * Math.pow(Math.E, -(Math.pow((x - 350), 2)) / 20000) * 125000 - 500);
+            graphics.drawLine(x, (int) y, x+1,(int) y+1);
         }
     }
 }
