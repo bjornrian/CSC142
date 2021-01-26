c8d2cfcca0522b366c105f423c2234f804f293c0
Egil Rian <egil.rian@expeditors.com>
2021-01-26 04:08:58 -0800

    estimateArea() is done, except for a couple bugs where 4 numbers should be shown as decimals but sometimes this doesn't happen (minor bug).

--- src/NormalDistribution/Calculations.java
+++ src/NormalDistribution/Calculations.java
@@ -8,14 +8,20 @@ public class Calculations {
         DecimalFormat decimalFormat = new DecimalFormat(pattern);
         double yValue = (1/(100 * (Math.sqrt(2 * Math.PI))) * Math.pow(Math.E, - (Math.pow((x - 350), 2)) / 20000));
         String format = decimalFormat.format(yValue);
-        System.out.println("The y-value attributed to this x-value is " + format);
+        System.out.println("The y-value attributed to this x-value is: " + format);
 
     }
 
     public static void estimateArea(int numStdDev) {
+        double shadedArea = 0;
         for (int x = 350 - numStdDev * 100; x <= 350 + numStdDev * 100; x++) {
             double yValue = (1/(100 * (Math.sqrt(2 * Math.PI))) * Math.pow(Math.E, - (Math.pow((x - 350), 2)) / 20000));
+            shadedArea += yValue;
         }
+        String pattern = "##.####";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+        String format = decimalFormat.format(shadedArea);
+        System.out.println("The estimated area with " + numStdDev + " standard deviation/s is: ~" + format);
     }
 }
 
