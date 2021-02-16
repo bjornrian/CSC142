c863fcdd2ecb0137d6b46b67157222bdf1c57bda
Egil Rian <egil.rian@expeditors.com>
2021-02-16 03:37:00 -0800

    WeatherManager.java is started, although I am confused about instantiating the main array.

--- src/seattleWeather/WeatherManager.java
+++ src/seattleWeather/WeatherManager.java
@@ -1,4 +1,11 @@
 package seattleWeather;
+import java.io.File;
 
 public class WeatherManager {
+    private WeatherDay[] weatherData = new WeatherDay[7];
+
+    File weatherFile = new File("SeattleWeather.txt");
+    public WeatherManager(File weatherFile){
+        
+    }
 }
